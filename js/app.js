78bbd081a6d1c34805be6c341c55be149a02cb69
Andrew Tang <actang150@gmail.com>
2017-05-31 00:58:06 -0500

    Fix styling of app.js.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 window.onload = function() {
     var config = {
-                    keyMap:
-                    {
-                        LEFT:37,
-                        RIGHT:39,
-                        DOWN:40,
-                        DROP:32,
-                        RTURN:38,
-                        LTURN:90,
-                        NEW:78,
-                        PAUSE:80,
-                        QUIT:81
-                    },
-                    containerId:'zone',
-                    tetris: 
-                    {
-                        initialDropRate: 500
-                    }           
-                };
+        keyMap:
+            {
+                LEFT:37,
+                RIGHT:39,
+                DOWN:40,
+                DROP:32,
+                RTURN:38,
+                LTURN:90,
+                NEW:78,
+                PAUSE:80,
+                QUIT:81
+            },
+        containerId:'zone',
+        tetris: 
+            {
+                dropPeriod: 250,
+                pieceFreq: 1,
+                height: 24
+            }           
+    };
+    window.console.lag = window.console.log;
+    window.console.log = function(){};
     window.game = new Engine(InputManager, GraphicsManager, Tetris, Queue, config);
 }
