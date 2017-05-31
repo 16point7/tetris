ee2435ea40339827d81f713ca36c16223015757c
Andrew Tang <actang150@gmail.com>
2017-05-31 01:21:18 -0500

    Add ability for graphicsmanager to recieve data object from tetris.js. Add ability for graphicsmanager to recieve notice of last frame to render.

--- js/graphicsmanager.js
+++ js/graphicsmanager.js
@@ -1,5 +1,6 @@
 function GraphicsManager() {
     this.div;       // DOM object to house the game
+    this.lastFrame  // boolean signifying that this is the last render
 }
 
 /* Defines location to inject the canvases */
@@ -14,12 +15,33 @@ GraphicsManager.prototype.initialize = function() {
 
 /* Clears the gameplay canvases, but not the static canvases */
 GraphicsManager.prototype.newState = function() {
+    this.lastFrame = false;
 }
 
 /* Display end-game graphics */
 GraphicsManager.prototype.endState = function() {
+    this.lastFrame = true;
 }
 
 /* Re-renders canvases that have changed */
-GraphicsManager.prototype.render = function(data, delta) {
+GraphicsManager.prototype.render = function(data, delta) {    
+    if (data.active.dirty) {
+        // render canvas 1
+        data.active.dirty = false;
+    }
+    if (data.static.dirty) {        
+        // render canvas 2
+        data.static.dirty = false;
+    }
+    if (data.score.dirty) {
+        // render canvas 3
+        data.score.dirty = false;
+    }
+    if (data.next.dirty) {
+        // render canvas 4
+        data.next.dirty = false;
+    }
+    if (this.lastFrame) {
+        // render end-game message
+    }
 }
