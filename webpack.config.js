df6e73f4777c947f208a1ead7498f013fbc6c055
Andrew Tang <actang150@gmail.com>
2017-06-24 15:17:02 -0500

    Setup webpack to bundle and inline css and move image dependencies to bin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 var path = require('path');
 var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+var StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, './bin/'),
+        path: path.resolve(__dirname, 'bin/'),
         filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                })
+            },
+            {
+                test:/\.(png|jpg|svg|gif)$/,
+                use: 'file-loader'
+            }
+        ]
     },
     plugins: [
         new UglifyJSPlugin({
@@ -22,6 +38,8 @@ module.exports = {
                 minifyCSS: true
             }
         }),
+        new ExtractTextPlugin('styles.css'),
+        new StyleExtHtmlWebpackPlugin(),
         new ScriptExtHtmlWebpackPlugin({
             inline: 'app.bundle.js'
         })
